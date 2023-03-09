a5b6112568637af38e86cebc2b79278fda200a19
AishwaryaRajput09 <aishwaryanaglot9144@gmail.com>
2023-03-10 02:34:31 +0530

    creating homepage layout

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/Layouts/Layout'
+
+function HomePage() {
+  return (
+    <Layout>
+        <div className="filter">
+          <div>range filters</div>
+          </div>
+          <div className="content">
+
+          </div>
+    </Layout>
+  )
+}
+
+export default HomePage
